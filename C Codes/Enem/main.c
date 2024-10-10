1736f3bc3ecd279d4c222bf14614742f8124c66b
ArthurV10 <arthurvms10@gmail.com>
2024-10-10 13:20:15 -0300

    feat: inicializated enem question in C

---
+++ C Codes/Enem/main.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+void createMatrix(char *matrix, int *rows, int *colunm){
+    
+}
+
+
+int main(void){
+    FILE *archive;
+    char **matrix;
+    
+    archive = fopen("./files/enem2014_nota_por_escola.txt.csv", "r");
+    return 0;
+}
+
+
+/*
+Top N Brasil (todas as áreas)
+Top N Brasil por Área
+Top N por Estado
+Top N por Estado e Rede (pública ou privada)
+Media Nacional por Área
+Melhor escola por Área e Estado ou BR
+Listas Escolas por Estado Ordenada Por Renda
+Busca escola específica por parte  Nome
+Ranking ENEM por Estado
+Ranking ENEM por Região do País
+
+
+*/
