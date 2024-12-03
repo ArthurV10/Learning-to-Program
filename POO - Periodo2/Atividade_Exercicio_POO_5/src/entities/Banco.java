711a123dd7f0c297439c95d3dde8299e3a3b02df
Arthur <arthurvms10@gmail.com>
2024-12-02 21:32:01 -0300

    feat: initializated new question in POO, homework 5 part1

--- POO - Periodo2/Atividade_Exercicio_POO_5/src/entities/Banco.java
+++ POO - Periodo2/Atividade_Exercicio_POO_5/src/entities/Banco.java
@@ -1,4 +1,79 @@
 package entities;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Banco {
+    private List<Conta> contas;
+    private List<Cliente> clientes;
+
+    public Banco() {
+        this.contas = new ArrayList<>();
+        this.clientes = new ArrayList<>(); // Inicialização da lista de clientes
+    }
+
+    public void inserir(Conta conta) {
+        this.contas.add(conta);
+    }
+
+    public List<Conta> getContas(){
+        return this.contas;
+    }
+
+    public List<Cliente> getClientes(){
+        return this.clientes;
+    }
+
+    public Conta consultar(String numero) {
+        for (Conta conta : getContas()) {
+            if (conta.getNumero().equals(numero)) {
+                return conta;
+            }
+        }
+        return null;
+    }
+
+    public void inserirCliente(Cliente cliente) {
+        boolean cpfExistente = false;
+        for (Cliente c : this.clientes){
+            if (cliente.getCpf().equals(cliente.getCpf())){
+               cpfExistente = true;
+               break;
+            }
+            if(!cpfExistente){
+                getClientes().add(cliente);
+            }
+        }
+    }
+
+    public Cliente consultarCliente(String cpf) {
+        for (Cliente cliente : getClientes()) {
+            if (cliente.getCpf().equals(cpf)) {
+                return cliente;
+            }
+        }
+        return null;
+    }
+
+    public Conta consultarConta(String numeroConta) {
+        for (Conta conta : getContas()) {
+            if (conta.getNumero().equals(numeroConta)) {
+                return conta;
+            }
+        }
+        return null;
+    }
+
+    public void associarContaCliente(String numeroConta, String cpfCliente) {
+        Conta contaEncontrada = consultarConta(numeroConta);
+        Cliente clienteEncontrado = consultarCliente(cpfCliente);
+
+        if (contaEncontrada.getCliente().getCpf() == cpfCliente){
+            System.out.println("Conta existente encontrada: " + numeroConta);
+            return;
+        }
+
+        contaEncontrada.setCliente(clienteEncontrado);
+        System.out.println("Cliente " + cpfCliente + " associado à conta " + numeroConta + " com sucesso.");
+    }
 }
--- POO - Periodo2/Atividade_Exercicio_POO_5/src/entities/Cliente.java
+++ POO - Periodo2/Atividade_Exercicio_POO_5/src/entities/Cliente.java
@@ -1,22 +1,51 @@
 package entities;
 
 import java.util.Date;
-import entities.Conta;
+import java.util.List;
 
 public class Cliente {
-    int id;
-    String nome;
-    String cpf;
-    Date dataNascimento;
-    Conta[] contas;
+    private int id;
+    private String nome;
+    private String cpf;
+    private Date dataNascimento;
+    private List<Conta> contas;
 
     public Cliente (int id, String nome,String cpf,Date dataNascimento, Conta contas){
         this.id = id;
         this.nome = nome;
         this.cpf = cpf;
         this.dataNascimento = dataNascimento;
-        this.contas.append(contas);
     }
 
-    public
+    public int getId() {
+        return this.id;
+    }
+
+    public String getCpf(){
+        return this.cpf;
+    }
+
+    public void setConta(Conta contaCliente){
+        this.contas.add(contaCliente);
+    }
+
+    public List<Conta> listarContasCliente() {
+        if (contas != null) {
+            for (int i = 0; i < this.contas.size(); i++) {
+                System.out.println("Conta " + (i + 1) + ": " + contas.get(i));
+            }
+            return this.contas;
+        }
+        System.out.println("Contas Inexistentes");
+        return null;
+    }
+
+    public float totalizarSaldoCliente(){
+        float valorTotal = 0;
+        for (Conta contaCliente : this.contas){
+            valorTotal += contaCliente.consultarSaldo();
+        }
+        return valorTotal;
+    }
 }
+
